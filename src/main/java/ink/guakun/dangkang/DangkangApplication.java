2f0ff4445aed612f78da2d672e57379b884c24cc
xiaogua07 <xiaogua07@outlook.com>
2024-04-23 21:10:08 +0800

    Initial project.

---
+++ src/main/java/ink/guakun/dangkang/DangkangApplication.java
@@ -0,0 +1,13 @@
+package ink.guakun.dangkang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DangkangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DangkangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ink/guakun/dangkang/DangkangApplicationTests.java
@@ -0,0 +1,13 @@
+package ink.guakun.dangkang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DangkangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
